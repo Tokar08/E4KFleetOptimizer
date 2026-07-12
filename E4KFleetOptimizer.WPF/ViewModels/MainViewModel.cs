d1050e89600e511ef4ad28d39894c2d7f62fdd6f
Tokar08 <ivan.tokar.git@gmail.com>
2026-07-12 22:45:10 +0200

    chore: setup MainViewModel skeleton and inject IFleetOptimizationService

---
+++ E4KFleetOptimizer.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using E4KFleetOptimizer.Core.Services;
+
+namespace E4KFleetOptimizer.WPF.ViewModels;
+
+public class MainViewModel
+{
+    private readonly IFleetOptimizationService _optimizationService;
+
+    public MainViewModel(IFleetOptimizationService optimizationService)
+    {
+        _optimizationService = optimizationService;
+    }
+}
