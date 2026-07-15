0d0f4cfbf594ad85e55a54efafe3bab26cb481d4
Tokar08 <ivan.tokar.git@gmail.com>
2026-07-15 22:47:58 +0200

    feat: wire up CalculateBestPath logic to CalculateOptimization command

--- E4KFleetOptimizer.WPF/ViewModels/MainViewModel.cs
+++ E4KFleetOptimizer.WPF/ViewModels/MainViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using E4KFleetOptimizer.Core.Models; 
 using E4KFleetOptimizer.Core.Services;
 
 namespace E4KFleetOptimizer.WPF.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
-
     private readonly IFleetOptimizationService _optimizationService;
 
     [ObservableProperty]
@@ -14,8 +15,25 @@ public partial class MainViewModel : ObservableObject
     [ObservableProperty]
     private int _maxIslandSlots;
 
+    [ObservableProperty]
+    private OptimizationResult? _calculationResult;
+
+    [ObservableProperty]
+    private List<int> _currentShips = new();
+
     public MainViewModel(IFleetOptimizationService optimizationService)
     {
         _optimizationService = optimizationService;
+    }
+
+    [RelayCommand]
+    private void CalculateOptimization()
+    {
+        if (Budget <= 0 || MaxIslandSlots <= 0)
+        {
+            return;
+        }
+
+        CalculationResult = _optimizationService.CalculateBestPath(Budget, CurrentShips, MaxIslandSlots);
     }
 }
