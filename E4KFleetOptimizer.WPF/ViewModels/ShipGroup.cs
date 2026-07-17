6cba358fdce9039e89f2b0908e60c53fa91303fb
Tokar08 <ivan.tokar.git@gmail.com>
2026-07-17 21:14:55 +0200

    feat: add ShipGroup model for UI grouping

---
+++ E4KFleetOptimizer.WPF/ViewModels/ShipGroup.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace E4KFleetOptimizer.WPF.ViewModels;
+
+public partial class ShipGroup : ObservableObject
+{
+    [ObservableProperty]
+    private int _level;
+
+    [ObservableProperty]
+    private int _count;
+}
