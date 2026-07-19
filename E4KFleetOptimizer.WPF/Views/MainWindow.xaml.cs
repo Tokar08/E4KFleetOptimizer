64aca43d61969ef541a525d67b18ada2ec97b74d
Tokar08 <ivan.tokar.git@gmail.com>
2026-07-20 00:52:59 +0200

    refactor: remove test code for ships.json from MainWindow

--- E4KFleetOptimizer.WPF/Views/MainWindow.xaml.cs
+++ E4KFleetOptimizer.WPF/Views/MainWindow.xaml.cs
@@ -11,28 +11,5 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = viewModel;
-        try
-        {
-            var provider = new JsonShipDataProvider();
-            var data = provider.GetLevelData().ToList();
-
-
-            string basePath = AppContext.BaseDirectory;
-            string filePath = System.IO.Path.Combine(basePath, "Resources", "ships.json");
-
-            MessageBox.Show(
-                $"Файл успешно считан: \n\nВесь путь:\n{filePath}\n\nЗагружено уровней кораблей: {data.Count}",
-                "Успех",
-                MessageBoxButton.OK,
-                MessageBoxImage.Information);
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show(
-                $"Произошла ошибка при чтении:\n{ex.Message}",
-                "Ошибка",
-                MessageBoxButton.OK,
-                MessageBoxImage.Error);
-        }
     }
 }
