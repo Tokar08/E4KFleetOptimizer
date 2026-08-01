249c98a423e086d816daf52f25c6863ef42276f8
Tokar08 <ivan.tokar.git@gmail.com>
2026-08-01 16:55:23 +0200

    feat(ui): add About tab and global input validation, fix results visibility on tab switch

--- E4KFleetOptimizer.WPF/Views/MainWindow.xaml.cs
+++ E4KFleetOptimizer.WPF/Views/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using E4KFleetOptimizer.WPF.Data;
-using E4KFleetOptimizer.WPF.ViewModels;
+﻿using E4KFleetOptimizer.WPF.ViewModels;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Navigation;
 
 
 namespace E4KFleetOptimizer.WPF;
@@ -19,5 +21,33 @@ public partial class MainWindow : Window
     private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
     {
         e.Handled = _numericRegex.IsMatch(e.Text);
+    }
+
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = e.Uri.AbsoluteUri,
+            UseShellExecute = true
+        });
+        e.Handled = true;
+    }
+
+    private async void CopyEmail_Click(object sender, RoutedEventArgs e)
+    {
+        Clipboard.SetText("ivan.tokar.git@gmail.com");
+
+
+        if (sender is Button btn && btn.ToolTip is ToolTip toolTip) 
+        {
+            toolTip.PlacementTarget = btn;
+            toolTip.Content = "Скопировано!";
+            toolTip.IsOpen = true;
+
+            await Task.Delay(1500);
+
+            toolTip.IsOpen = false;
+            toolTip.Content = "Скопировать почту";
+        }
     }
 }
